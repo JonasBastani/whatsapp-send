366e756009b8daec6fa15464b5d95ee6b8581afd
Jonas Bastani <jonasbastani1999@gmail.com>
2022-12-09 16:40:09 -0300

    implemented save message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,17 @@ export class HomePage {
 
   public data = {
     number: '',
-    message: ''
+    message: '',
+    save: false
   }
 
   public valid = '';
 
   constructor() {}
+
+  ionViewWillEnter(){
+    this.checkMessage();
+  }
 
   send(){
     this.valid = '';
@@ -31,7 +37,36 @@ export class HomePage {
   }
 
   openWpp(message:string, number:string) {
+    this.checkToSave(); 
     window.open('https://api.whatsapp.com/send/?phone='+number+'&text='+encodeURI(message));
+  }
+
+  async save(message:string) {
+    await Storage.set({
+      key: 'message',
+      value: message,
+    });
+  };
+
+  async checkMessage(){
+    const { value } = await Storage.get({ key: 'message' });
+    if(value){
+      this.data.message = value;
+      this.data.save = true;
+    }
+  }
+
+  async checkToSave(){
+    const { value } = await Storage.get({ key: 'message' });
+    if(this.data.save){
+      this.save(this.data.message);
+    } else {
+      if(value) await Storage.remove({ key: 'message' });
+    }
+  }
+
+  changeSave(){
+    this.data.save = this.data.save ? false: true;
   }
 
 }
